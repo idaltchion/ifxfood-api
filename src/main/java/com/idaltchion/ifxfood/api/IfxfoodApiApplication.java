2de11d1864c233d5a8adc79cc491fddf4b38feb5
idaltchion <idaltchion@gmail.com>
2020-06-10 14:51:33 -0300

    First commit and initial mysql prepared

---
+++ src/main/java/com/idaltchion/ifxfood/api/IfxfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.idaltchion.ifxfood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfxfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfxfoodApiApplication.class, args);
+	}
+
+}
