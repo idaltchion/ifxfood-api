95dfeb35b198ff86cf3cc0576ca47e40690c5279
idaltchion <idaltchion@gmail.com>
2021-02-12 06:02:25 -0300

    Refactoring assembler and disassembler class to use Object Mapper library

--- src/main/java/com/idaltchion/ifxfood/api/assembler/RestauranteDTOAssembler.java
+++ src/main/java/com/idaltchion/ifxfood/api/assembler/RestauranteDTOAssembler.java
@@ -3,30 +3,24 @@ package com.idaltchion.ifxfood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.idaltchion.ifxfood.api.model.CozinhaDTO;
 import com.idaltchion.ifxfood.api.model.RestauranteDTO;
 import com.idaltchion.ifxfood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDTOAssembler {
 
+	@Autowired
+	private ModelMapper modelMapper;	
+	
 	/*
 	 * efetua a conversao do objeto de Modelo de Dominio para objeto de Modelo de Representacao (DTO de Saida)
 	 */
 	public RestauranteDTO toDTO(Restaurante restaurante) {
-		CozinhaDTO cozinhaDTO = new CozinhaDTO();
-		cozinhaDTO.setId(restaurante.getCozinha().getId());
-		cozinhaDTO.setNome(restaurante.getCozinha().getNome());
-		
-		RestauranteDTO restauranteDTO = new RestauranteDTO();
-		restauranteDTO.setId(restaurante.getId());
-		restauranteDTO.setNome(restaurante.getNome());
-		restauranteDTO.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteDTO.setCozinha(cozinhaDTO);
-		
-		return restauranteDTO;
+		return modelMapper.map(restaurante, RestauranteDTO.class);
 	}
 	
 	public List<RestauranteDTO> toCollectionDTO(List<Restaurante> restaurantes) {
--- src/main/java/com/idaltchion/ifxfood/api/assembler/RestauranteDTODisassembler.java
+++ src/main/java/com/idaltchion/ifxfood/api/assembler/RestauranteDTODisassembler.java
@@ -1,28 +1,23 @@
 package com.idaltchion.ifxfood.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.idaltchion.ifxfood.api.model.input.RestauranteDTOInput;
-import com.idaltchion.ifxfood.domain.model.Cozinha;
 import com.idaltchion.ifxfood.domain.model.Restaurante;
 
 @Component
 public class RestauranteDTODisassembler {
 
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	/*
 	 * efetua a conversao do objeto de Modelo de Representacao (DTO de Entrada) para Modelo de Dominio
 	 */
 	public Restaurante toDomain(RestauranteDTOInput restauranteInput) {
-		Restaurante restaurante = new Restaurante();
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-		
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-		
-		restaurante.setCozinha(cozinha);
-		
-		return restaurante;
+		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 	
 }
