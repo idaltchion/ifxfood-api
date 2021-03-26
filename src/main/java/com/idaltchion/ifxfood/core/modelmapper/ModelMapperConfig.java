13632e589ce4b66cb14d2b3494bd63852381329d
idaltchion <idaltchion@gmail.com>
2021-03-26 05:53:58 -0300

    Change resource RestauranteDTO to show EnderecoDTO instead Endereco

--- src/main/java/com/idaltchion/ifxfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/idaltchion/ifxfood/core/modelmapper/ModelMapperConfig.java
@@ -4,12 +4,22 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.idaltchion.ifxfood.api.model.EnderecoDTO;
+import com.idaltchion.ifxfood.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		var modelMapper = new ModelMapper();
+		
+		var enderecoToEnderecoDTOMap = modelMapper.createTypeMap(Endereco.class, EnderecoDTO.class);
+		enderecoToEnderecoDTOMap.<String>addMapping(
+				fromEndereco -> fromEndereco.getCidade().getEstado().getNome(), 
+				(toEnderecoDTO, valor) -> toEnderecoDTO.getCidade().setEstado(valor));
+		
+		return modelMapper;
 	}
 	
 }
