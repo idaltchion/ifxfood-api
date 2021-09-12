390e4a38a3c4cd57a34683822ef9256adcbe39d9
idaltchion <idaltchion@gmail.com>
2021-09-12 10:59:32 -0300

    feat(order): add Domain Event concept when an order is confirmed

---
+++ src/main/java/com/idaltchion/ifxfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -0,0 +1,31 @@
+package com.idaltchion.ifxfood.domain.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import com.idaltchion.ifxfood.domain.event.PedidoConfirmadoEvent;
+import com.idaltchion.ifxfood.domain.model.Pedido;
+import com.idaltchion.ifxfood.domain.service.EnvioEmailService;
+import com.idaltchion.ifxfood.domain.service.EnvioEmailService.Mensagem;
+
+@Component
+public class NotificacaoClientePedidoConfirmadoListener {
+
+	@Autowired
+	private EnvioEmailService envioEmail;
+	
+	@TransactionalEventListener
+	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
+		Pedido pedido = event.getPedido();
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("pedido-confirmado.html")
+				.variavel("pedido", pedido)
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+
+		envioEmail.enviar(mensagem);
+	}
+
+}
