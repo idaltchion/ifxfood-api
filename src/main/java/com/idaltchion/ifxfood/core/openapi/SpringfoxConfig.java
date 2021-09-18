8f763616d16e1c3e3f5603b044859edc60e1e79a
idaltchion <idaltchion@gmail.com>
2021-09-18 16:29:17 -0300

    feat(doc): add springfox starter and configure project to enable swagger for controllers

---
+++ src/main/java/com/idaltchion/ifxfood/core/openapi/SpringfoxConfig.java
@@ -0,0 +1,31 @@
+package com.idaltchion.ifxfood.core.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringfoxConfig implements WebMvcConfigurer {
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.idaltchion.ifxfood.api.controller"))
+				.build();
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("index.html")
+			.addResourceLocations("classpath:META-INF/resources/webjars/springfox-swagger-ui/**");
+	}
+	
+}
