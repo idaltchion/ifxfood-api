d7ead6e683f6cbeca408d57e014f27a0fc1165e6
idaltchion <idaltchion@gmail.com>
2021-09-19 16:24:26 -0300

    feat(doc): set Problem class on response of doc for both http status 400 and 500

--- src/main/java/com/idaltchion/ifxfood/api/exceptionhandler/Problem.java
+++ src/main/java/com/idaltchion/ifxfood/api/exceptionhandler/Problem.java
@@ -9,25 +9,47 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+@ApiModel(description = "Representação de um problema")
 @JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
+	
+	@ApiModelProperty(position = 5)
 	private OffsetDateTime timestamp;
+	
+	@ApiModelProperty(example = "400", position = 10)
 	private Integer status;
+	
+	@ApiModelProperty(example = "https://ifxfood.com.br/dado-invalido", position = 15)
 	private String type;
+	
+	@ApiModelProperty(example = "Dado inválido", position = 20)
 	private String title;
+	
+	@ApiModelProperty(example = "Um ou mais campos estão inválidos. Preencha corretamente e tente novamente.", position = 25)
 	private String detail;
+	
+	@ApiModelProperty(example = "Um ou mais campos estão inválidos. Preencha corretamente e tente novamente.", position = 30)
 	private String userMessage;
+	
+	@ApiModelProperty(position = 35)
 	private List<Field> fields;
 	
+	@ApiModel(description = "Representação de campo com problema (opcional)")
 	@Getter
 	@Builder
 	public static class Field {
+		
+		@ApiModelProperty(example = "preco")
 		private String name;
+		
+		@ApiModelProperty(example = "O preenchimento do campo preco é obrigatório")
 		private String userMessage;
 	}
 }
--- src/main/java/com/idaltchion/ifxfood/core/openapi/SpringfoxConfig.java
+++ src/main/java/com/idaltchion/ifxfood/core/openapi/SpringfoxConfig.java
@@ -2,15 +2,21 @@ package com.idaltchion.ifxfood.core.openapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.apache.http.HttpStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.idaltchion.ifxfood.api.exceptionhandler.Problem;
+
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RepresentationBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -33,56 +39,102 @@ public class SpringfoxConfig implements WebMvcConfigurer {
 	
 	@Bean
 	public Docket apiDocket() {
+		var typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.OAS_30)
-				.select()
-					.apis(RequestHandlerSelectors.basePackage("com.idaltchion.ifxfood.api"))
-					.build()
-				.apiInfo(apiInfo())
-				.tags(tags()[0], tags())
-				.useDefaultResponseMessages(false)
-				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
-				.globalResponses(HttpMethod.POST, globalPostResponseMessages())
-				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
-				.globalResponses(HttpMethod.PUT, globalPutResponseMessages());
-	}
-	
-	private List<Response> globalPutResponseMessages() {
-		return Arrays.asList(
-				responseMessage(HttpStatus.SC_BAD_REQUEST, "Bad Request"),
-				responseMessage(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal Server Error"),
-				responseMessage(HttpStatus.SC_NOT_ACCEPTABLE, "Not Acceptable"),
-				responseMessage(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type")
-		);
-	}
-
-	private List<Response> globalDeleteResponseMessages() {
-		return Arrays.asList(
-				responseMessage(HttpStatus.SC_BAD_REQUEST, "Bad Request"),
-				responseMessage(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal Server Error")
-		);
-	}
-
-	private List<Response> globalPostResponseMessages() {
-		return Arrays.asList(
-				responseMessage(HttpStatus.SC_BAD_REQUEST, "Bad Request"),
-				responseMessage(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal Server Error"),
-				responseMessage(HttpStatus.SC_NOT_ACCEPTABLE, "Not Acceptable"),
-				responseMessage(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type")
-		);
+			.select()
+				.apis(RequestHandlerSelectors.basePackage("com.idaltchion.ifxfood.api"))
+				.build()
+			.useDefaultResponseMessages(false)
+			.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+			.globalResponses(HttpMethod.POST, globalPostResponseMessages())
+			.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+			.globalResponses(HttpMethod.PUT, globalPutResponseMessages())
+			.additionalModels(typeResolver.resolve(Problem.class))
+			.apiInfo(apiInfo())
+			.tags(tags()[0], tags());
 	}
 	
 	private List<Response> globalGetResponseMessages() {
 		return Arrays.asList(
-				responseMessage(HttpStatus.SC_INTERNAL_SERVER_ERROR, "Internal Server Error"),
-				responseMessage(HttpStatus.SC_NOT_ACCEPTABLE, "Not Acceptable")
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR))
+				.description("Internal Server Error")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build(),
+			responseMessage(HttpStatus.SC_NOT_ACCEPTABLE, "Not Acceptable")
 		);
 	}
 	
+	private List<Response> globalPostResponseMessages() {
+		return Arrays.asList(
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_BAD_REQUEST))
+				.description("Bad Request")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build(),
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR))
+				.description("Internal Server Error")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build(),
+			responseMessage(HttpStatus.SC_NOT_ACCEPTABLE, "Not Acceptable"),
+			responseMessage(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type")
+		);
+	}
+	
+	private List<Response> globalDeleteResponseMessages() {
+		return Arrays.asList(
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_BAD_REQUEST))
+				.description("Bad Request")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build(),
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR))
+				.description("Internal Server Error")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build()
+		);
+	}
+	
+	private List<Response> globalPutResponseMessages() {
+		return Arrays.asList(
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_BAD_REQUEST))
+				.description("Bad Request")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build(),
+			new ResponseBuilder()
+				.code(String.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR))
+				.description("Internal Server Error")
+				.representation(MediaType.APPLICATION_JSON).apply(representationBuilderProblem())
+				.build(),
+			responseMessage(HttpStatus.SC_NOT_ACCEPTABLE, "Not Acceptable"),
+			responseMessage(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type")
+		);
+	}
+
 	private Response responseMessage(int httpStatusCode, String httpMessage) {
 		return new ResponseBuilder()
-				.code(String.valueOf(httpStatusCode))
-				.description(httpMessage)
-				.build();
+			.code(String.valueOf(httpStatusCode))
+			.description(httpMessage)
+			.build();
+	}
+	
+	/*
+	 * Método que linka a representacao da classe Problem (na resposta do swagger) com o verbo http que o chama  
+	 */
+	private Consumer<RepresentationBuilder> representationBuilderProblem() {
+		return r -> r.model(
+				m -> m.name("Problem").referenceModel(
+						ref -> ref.key(
+								k -> k.qualifiedModelName(
+										q -> q.name("Problem").namespace("com.idaltchion.ifxfood.api.exceptionhandler")
+								)
+						)
+					)
+				);
 	}
 
 	private Tag[] tags() {
