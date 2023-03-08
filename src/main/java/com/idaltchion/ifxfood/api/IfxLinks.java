3b9d490569d4e437d5217e584642c152f1af1a3d
idaltchion <idaltchion@gmail.com>
2023-03-07 21:28:01 -0300

    feat(hateoas): add template variables on links of Pedidos

---
+++ src/main/java/com/idaltchion/ifxfood/api/IfxLinks.java
@@ -0,0 +1,35 @@
+package com.idaltchion.ifxfood.api;
+
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.hateoas.TemplateVariable.VariableType;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Component;
+
+import com.idaltchion.ifxfood.api.controller.PedidoController;
+
+@Component
+public class IfxLinks {
+
+	public static final TemplateVariables PAGE_VARIABLES = new TemplateVariables(
+			new TemplateVariable("page", VariableType.REQUEST_PARAM),
+			new TemplateVariable("size", VariableType.REQUEST_PARAM),
+			new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+	
+	public Link linkToPedidos() {
+		String pedidosUrl = WebMvcLinkBuilder.linkTo(PedidoController.class).toUri().toString();
+		
+		
+		TemplateVariables filterVariables = new TemplateVariables(
+				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
+		
+		return Link.of(UriTemplate.of(pedidosUrl, filterVariables.concat(PAGE_VARIABLES)), IanaLinkRelations.COLLECTION);
+	}
+	
+}
--- src/main/java/com/idaltchion/ifxfood/api/assembler/PedidoDTOAssembler.java
+++ src/main/java/com/idaltchion/ifxfood/api/assembler/PedidoDTOAssembler.java
@@ -2,11 +2,11 @@ package com.idaltchion.ifxfood.api.assembler;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
+import com.idaltchion.ifxfood.api.IfxLinks;
 import com.idaltchion.ifxfood.api.controller.CidadeController;
 import com.idaltchion.ifxfood.api.controller.FormaPagamentoController;
 import com.idaltchion.ifxfood.api.controller.PedidoController;
@@ -21,6 +21,9 @@ public class PedidoDTOAssembler extends RepresentationModelAssemblerSupport<Pedi
 
 	@Autowired
 	private ModelMapper modelMapper;
+	
+	@Autowired
+	private IfxLinks ifxLinks;
 
 	public PedidoDTOAssembler() {
 		super(PedidoController.class, PedidoDTO.class);
@@ -30,7 +33,7 @@ public class PedidoDTOAssembler extends RepresentationModelAssemblerSupport<Pedi
 	public PedidoDTO toModel(Pedido pedido) {
 		PedidoDTO pedidoDTO = createModelWithId(pedido.getCodigo(), pedido);
 		modelMapper.map(pedido, pedidoDTO);
-		
+				
 		pedidoDTO.getFormaPagamento().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(FormaPagamentoController.class).buscar(pedidoDTO.getFormaPagamento().getId())).withSelfRel());
 		pedidoDTO.getEnderecoEntrega().getCidade().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(CidadeController.class).buscar(pedidoDTO.getEnderecoEntrega().getCidade().getId())).withSelfRel());
 		pedidoDTO.getRestaurante().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class).buscar(pedidoDTO.getRestaurante().getId())).withSelfRel());
@@ -46,7 +49,7 @@ public class PedidoDTOAssembler extends RepresentationModelAssemblerSupport<Pedi
 	
 	public PedidoDTO toModelWithCollectionRel(Pedido pedido) {
 		PedidoDTO pedidoDTO = toModel(pedido);
-		pedidoDTO.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel(IanaLinkRelations.COLLECTION));
+		pedidoDTO.add(ifxLinks.linkToPedidos());
 		
 		return pedidoDTO;
 	}
