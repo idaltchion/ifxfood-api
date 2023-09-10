3005e4885a6000caa065170b4ff250007458fc3f
idaltchion <idaltchion@gmail.com>
2023-09-10 20:46:34 -0300

    feat(security): configure user and password (in memory for while) to authenticate using http basic

--- src/main/java/com/idaltchion/ifxfood/core/security/WebSecurityConfig.java
+++ src/main/java/com/idaltchion/ifxfood/core/security/WebSecurityConfig.java
@@ -1,15 +1,31 @@
 package com.idaltchion.ifxfood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+				.password(passwordEncoder().encode("12345"))
+				.roles("ADMIN")
+			.and()
+			.withUser("user")
+				.password(passwordEncoder().encode("12345"))
+				.roles("USUARIO");
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		/*
@@ -29,6 +45,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 					.sessionCreationPolicy(SessionCreationPolicy.STATELESS) //4
 			.and()
 				.csrf().disable(); //5
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
